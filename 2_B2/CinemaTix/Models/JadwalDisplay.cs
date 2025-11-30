f59165d50f73210ae1871f94198cfca2568c33de
Gideon H.S <oficialgideon@gmail.com>
2025-11-30 13:33:33 +0700

    Add files via upload

---
+++ 2_B2/CinemaTix/Models/JadwalDisplay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTix.Models
+{
+    // Tambahkan di bawah FormPilihJadwal class atau di file Models/JadwalDisplay.cs
+    public class JadwalDisplay
+    {
+        public int JadwalID { get; set; }
+        public string DisplayText { get; set; }
+        public string NamaStudio { get; set; }
+        // Anda juga bisa menambahkan Harga di sini jika diperlukan
+    }
+}
---
+++ 2_B2/CinemaTix/Models/KursiDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTix.Models
+{
+    public class KursiDetail
+    {
+        public int KursiID { get; set; }
+        public string Baris { get; set; }
+        public int Nomor { get; set; }
+        public string StatusKursi { get; set; } // 'Tersedia', 'Terisi'
+        public bool IsSelected { get; set; } // Status untuk aplikasi (user sedang memilih)
+    }
+}
